1bc45a3eacb96db4382636bef4e2282864fee991
Khushi Tulsiyan <khushitulsiyan495@gmail.com>
2021-01-22 20:44:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,215 @@
+                        <!DOCTYPE html>
+                        <html>
+                        <head>
+                            <title>Alpha Tutorials</title>
+
+
+                             <meta charset="utf-8">
+
+                          <meta name="viewport" content="width=device-width, initial-scale=1">
+                            <meta name="Keywords" content="RxJS, ggplot2, Python Data Persistence, Caffe2, PyBrain, Python Data Access, H2O, Colab, Theano, Flutter, KNime, Mean.js, Weka, Solidity"/>
+		<link rel="canonical" href="https://www.tutorialspoint.com" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<link rel="stylesheet" href="https://www.tutorialspoint.com/themes/css/home.css?v=.12" />   
+		<link rel="stylesheet" href="https://store.tutorialspoint.com/css/pdf.css?v=3.3.2" />   
+		<link rel="stylesheet" href="https://www.tutorialspoint.com/videotutorials/css/videos.css?v=2.3.5" />   
+		<script src="https://www.tutorialspoint.com/themes/js/jquery.min.js"></script>
+		<script src="https://www.tutorialspoint.com/videotutorials/js/fontawesome.js"></script>
+		<script src="https://www.tutorialspoint.com/videotutorials/js/mui-combined.min.js"></script>
+
+                            <link rel="stylesheet" type="text/css" href="style.css">
+
+                            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+                            <link href="https://fonts.googleapis.com/css2?family=Caveat&display=swap" rel="stylesheet">
+
+                            </head>
+                        <body>
+
+
+
+                        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                          <a class="navbar-brand" href="#">Alpha Tutorials</a>
+                          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                          </button>
+
+                          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ml-auto">
+                              <li class="nav-item active">
+                                <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                              </li>
+
+                                <li class="nav-item">
+                                <a class="nav-link" href="about.php">Latest Courses</a>2
+                              </li>
+
+                                <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Coding Ground</a>
+                              </li>
+
+                              <li class="nav-item">
+                                <a class="nav-link disabled" href="#">Q/A</a>
+                              </li>
+                                
+                                 <li class="nav-item">
+                                <a class="nav-link" href="about.php">Login</a>
+                              </li>
+
+
+                            </ul>
+                            <form class="form-inline my-2 my-lg-0">
+                              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                            </form>
+                          </div>
+                        </nav>
+
+                            <div class="tp-search-widget" style="padding: 50px 0px 0px 0px">         
+			<div class="mui-container" style="padding-left:0px; padding-right:0px;">
+				<div class="tp-logo">
+					<a href="https://www.tutorialspoint.com/index.htm" title="Alpha Tutoria;s"><img src="https://www.tutorialspoint.com/images/tp-logo-diamond.png" alt="Tutorialspoint" title="Alpha Tutorials"/></a>
+				</div>
+
+                               <h4>You are browsing the best resource for <b>Online Education</b></h4>
+                               <div id='search-box'>
+                                <script>
+                                  (function() {
+                                    var cx = '000263362972888060243:yfed6eu421q';
+                                    var gcse = document.createElement('script');
+                                    gcse.type = 'text/javascript';
+                                    gcse.async = true;
+                                    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+                                    var s = document.getElementsByTagName('script')[0];
+                                    s.parentNode.insertBefore(gcse, s);
+                                  })();
+                                </script>
+                                <gcse:search></gcse:search>
+                               </div>
+
+				<div class="tp-banner-links">
+					<a href="https://www.tutorialspoint.com/tutorialslibrary.htm"><i class="fa fa-cubes tp-banner-icons"></i> <span>Library</span></a> 
+					<a href="https://www.tutorialspoint.com/videotutorials/index.php"><i class="fa fa-video tp-banner-icons"></i> <span>Videos Courses</span></a>
+					<a href="https://store.tutorialspoint.com"><i class="fa fa-book tp-banner-icons"></i> <span>eBooks</span></a>
+					<a href="https://www.tutorialspoint.com/gate_exams_tutorials.htm"><i class="fa fa-check-square tp-banner-icons"></i> <span>GATE Exams</span></a>
+				</div>  
+			</div>     
+		</div> 
+
+                          <a class="carousel-control-prev" href="style.css" data-slide="prev">
+                            <span class="carousel-control-prev-icon"></span>
+                          </a>
+                          <a class="carousel-control-next" href="style.css" data-slide="next">
+                            <span class="carousel-control-next-icon"></span>
+                          </a>
+                        </div>
+
+
+                          
+                                     <section class="my-5">
+                                <div class="py-5">
+
+                                    <h2 class="text-center">LATEST COURSES</h2>
+                                         </div>
+
+                                         <div class="container-fluid">
+                                             <div class="row">
+                                                 <div class="col-lg-4 col-md-4 col-12">
+
+
+                                                     <div class="card">
+                  <img class="card-img-top" src="images/coffee.jpg" alt="Card image">
+                  <div class="card-body">
+                    <h4 class="card-title">Coffee</h4>
+                    <p class="card-text">Have some caffeine shots.</p>
+                    <a href="#" class="btn btn-primary">See Menu</a>
+                  </div>
+                                                     </div>
+                                                 </div>
+
+                                                     <div class="col-lg-4 col-md-4 col-12">
+                                                         <div class="card" >
+                  <img class="card-img-top" src="images/tea.jpg" alt="Card image">
+                  <div class="card-body">
+                    <h4 class="card-title">Tea</h4>
+                    <p class="card-text">There is no drink as refreshing as tea.</p>
+                    <a href="#" class="btn btn-primary">See Menu</a>
+                  </div>
+                                                         </div>
+                                                 </div>
+
+
+
+                                                              <div class="col-lg-4 col-md-4 col-12">
+                                                             <div class="card">
+                  <img class="card-img-top" src="images/muffin.jpg" alt="Card image">
+                  <div class="card-body">
+                    <h4 class="card-title">Breads</h4>
+                    <p class="card-text">You don't need any occasion for sweets.</p>
+                    <a href="#" class="btn btn-primary">See Menu</a>
+                  </div>
+                </div>
+
+
+
+
+
+
+                                                 </div>
+                                             </div>
+                                         </div>
+
+
+                                    </section>
+
+
+                                
+                                
+                                
+                                 <section class="my-5">
+                          
+                                     <div class="py-5">
+
+                              
+                                         <h2 class="text-center">GALLERY</h2>
+                                     </div>
+                                     
+                                                     
+                                     <div class="container-fluid">
+                                         <div class="row">
+                                             <div class="col-lg-4 col-md-4 col-12">
+                                             <img src="images/coffee-mug-1493946797-1538791870.jpg" class="img-fluid pb-3">
+                                                 
+                                             </div>
+                                             
+                                               <div class="col-lg-4 col-md-4 col-12">
+                                             <img src="images/coffee-mug-1493946797-1538791870.jpg" class="img-fluid pb-3">
+                                                 
+                                             </div>
+                                             
+                                             
+                                               <div class="col-lg-4 col-md-4 col-12">
+                                             <img src="images/coffee-mug-1493946797-1538791870.jpg" class="img-fluid pb-3">
+                                                 
+                                             </div>
+                                         </div>
+                                     </div>
+                                     
+                                     
+                                     
+                                     
+                                     
+                                     
+                                </section>
+
+
+
+
+                            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+                        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+                        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+                            </body>
+
+                        </html>
